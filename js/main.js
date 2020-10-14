762a744f5b2b6ee5a5bd253629a8609fa8480b63
Aleksandr <aleksandr@MacBook-Air.local>
2020-10-14 19:42:57 +0500

    Menu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,53 @@
-window.onload = function(){
+window.onload = function () {
+  // Движение вниз
+  let dropMenu__btn = document.querySelector('.dropMenu__btn');
+  let dropMenu__body = document.querySelector('.dropMenu__items');
+  dropMenu__btn.addEventListener('click', dropDownMenu);
+  function dropDownMenu(e) {
+    e.preventDefault();
+    this.classList.toggle('active');
+    if (this.classList.contains('active')) {
+      let trans = function () {
+        dropMenu__body.classList.remove('fa-enter');
+        dropMenu__body.removeEventListener('transitionend', trans);
+      }
+      dropMenu__body.style.display = 'block';
+      dropMenu__body.classList.add('enter');
+      raf(function () {
+        dropMenu__body.classList.add('fa-enter');
+        dropMenu__body.classList.remove('enter');
+      });
+      dropMenu__body.addEventListener('transitionend', trans);
+    } else {
+      let trans = function () {
+        dropMenu__body.style.display = 'none';
+        dropMenu__body.classList.remove('drop');
+        dropMenu__body.removeEventListener('transitionend', trans);
+      }
+      dropMenu__body.classList.add('drop');
+      dropMenu__body.addEventListener('transitionend', trans);
+    }
+  }
+// Движение в право
+  let linkParents = document.querySelectorAll('dropMenu__item_parent');
+  let subMenu__content = document.querySelector('.subMenu__content');
+  for(let i = 0; i < linkParents.length; i++){
+    const linkParent = linkParents[i];
+    linkParent.addEventListener('mouseenter', function(e){
+      e.preventDefault();
+      this.classList.add('act');
+      this>subMenu__content.classList.add('sh');
+    });
+    linkParent.addEventListener('mouseleave', function(e){
+      this.classList.remove('act');
+      this>subMenu__content.classList.remove('sh');
+    });
+  }
+}
+function raf(fn) {
+  window.requestAnimationFrame(function () {
+    window.requestAnimationFrame(function () {
+      fn();
+    });
+  });
 }
