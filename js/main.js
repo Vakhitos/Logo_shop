6c667908ffa628b7b41449f9372d986d26efece5
perslity <ale.vakhitov@gmail.com>
2020-10-19 22:00:52 +0500

    Верстка слайдера

--- js/main.js
+++ js/main.js
@@ -67,41 +67,60 @@ window.onload = function () {
   // ===== ФОРМА ФИЛЬТР =====
   let filterCategoriesBtn = document.querySelector('.filter-categories__btn');
   let filterCategoriesContent = document.querySelector('.filter-categories__content');
-  filterCategoriesBtn.addEventListener('click', function (e) {
+  let categoriesCheckox = document.querySelectorAll('.filter-categories__checkbox-category');
+
+  filterCategoriesBtn.addEventListener('click', filterCategoriesBtnClick);
+  function filterCategoriesBtnClick() {
     this.classList.toggle('active');
-    if (!filterCategoriesBtn.classList.contains('active')) {
-      filterCategoriesContent.classList.add('hide');
-      let handler = function () {
-        filterCategoriesContent.style.display = 'none';
-        filterCategoriesContent.classList.remove('hide');
-        filterCategoriesContent.removeEventListener('transitionend', handler);
-      }
-      filterCategoriesContent.addEventListener('transitionend', handler);
-    } else {
-      filterCategoriesContent.style.display = 'block';
-      filterCategoriesContent.classList.add('show-temp');
-      raf(function () {
-        filterCategoriesContent.classList.remove('show-temp');
-        filterCategoriesContent.classList.add('show');
-      });
-      let handler = function () {
-        filterCategoriesContent.classList.remove('show');
-        filterCategoriesContent.removeEventListener('transitionend', handler);
-      }
-      filterCategoriesContent.addEventListener('transitionend', handler);   
-    }
-  });
-
-  function raf(fn) {
-    window.requestAnimationFrame(function () {
-      window.requestAnimationFrame(function () {
-        fn();
-      });
+    raf(function(){
+       slideToggle(filterCategoriesContent, filterCategoriesBtn);
     });
+
   }
-
-
+  for (let index = 0; index < categoriesCheckox.length; index++) {
+    const categoryCheckox = categoriesCheckox[index];
+    categoryCheckox.addEventListener('change', function(){
+      categoryCheckox.classList.toggle('checkSelect');
+      let categoryCheckoxSelect = document.querySelectorAll('.filter-categories__checkbox-category.checkSelect');
+      if(categoryCheckoxSelect.length > 0){
+        filterCategoriesBtn.classList.add('select');
+        let categoryCheckoxQuantity = filterCategoriesBtn.querySelector('.filter-categories__title_qantity');
+        categoryCheckoxQuantity.innerHTML = categoryCheckoxQuantity.getAttribute('data-text') + ' ' +categoryCheckoxSelect.length;
+      }
+      else {
+        filterCategoriesBtn.classList.remove('select');
+      }
+    });
+    
+  }
 }
 
 // =====================================================
 
+// ===== Main Function =====
+function raf(fn){
+  window.requestAnimationFrame(function(){
+    window.requestAnimationFrame(function(){
+      fn();
+    });
+  });
+}
+//===============================================
+function slideToggle(block, btn) {
+  if (btn.classList.contains('active')) {
+    let handler = function () {
+      block.classList.remove('show');
+    }
+    block.classList.add('show-enter');
+    window.requestAnimationFrame(function () {
+      block.classList.remove('show-enter');
+      block.classList.add('show');
+    });
+    block.style.display = 'block';
+    block.addEventListener('transitionend', handler);
+  }
+  else {
+    block.style.display = 'none';
+  }
+}
+//===============================================
